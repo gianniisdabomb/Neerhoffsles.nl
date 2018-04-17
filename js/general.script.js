2d470394744a338e8e14b6b25b2b8c3950ef234a
Oscar Rijpstra <00276969@rijnijssel.nl>
2018-04-17 11:46:26 +0200

    added html template

---
+++ js/general.script.js
@@ -0,0 +1,33 @@
+FitNavigation();
+footer();
+
+window.addEventListener('resize', () =>{
+    footer();
+});
+
+function FitNavigation(){
+    const hamburgermenu = document.getElementById('burgermenu');
+    const navigation = document.getElementById('nav-container');
+
+    let navigationshowed = false;
+    hamburgermenu.addEventListener('click', () =>{
+    if (navigationshowed === false){
+        navigationshowed = true;
+        navigation.style.display = 'block';        
+    }
+    else{
+        navigationshowed = false;
+        navigation.style.display = 'none';
+    }
+    });
+}
+function footer(){
+    let hasVScroll = document.body.scrollHeight > window.innerHeight;
+    let footer = document.getElementById('footer');
+    if (hasVScroll == false){
+        footer.style.position = 'absolute';
+    }
+    else{
+        footer.style.position = 'relative';
+    }
+}
