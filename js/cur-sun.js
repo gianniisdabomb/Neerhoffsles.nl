f25b3b5d8af476dbebf7d38e2dd80436c93951e5
Oscar Rijpstra <00276969@rijnijssel.nl>
2018-04-22 18:35:25 +0200

    added all pages

--- js/cur-sun.js
+++ js/cur-sun.js
@@ -1,12 +1,12 @@
 const sun = document.getElementById('moving-sun').children[0];
 
 window.addEventListener('scroll', () =>{
-    if (document.documentElement.scrollTop < document.documentElement.clientWidth - 15){
-        setSunPosition();
-    }
+    setSunPosition();
 });
 
 function setSunPosition(){
-    sun.style.transform = 'translateX(' + document.documentElement.scrollTop + '%)';
-    sun.style.webkitTransform = 'translateX(' + document.documentElement.scrollTop + '%)';
+    if (document.documentElement.scrollTop < 300){
+        sun.style.transform = 'translateX(' + document.documentElement.scrollTop + 200 + '%)';
+    }
+    //sun.style.webkitTransform = 'translateX(' + document.documentElement.scrollTop * 1.3 + '%)';
 }
