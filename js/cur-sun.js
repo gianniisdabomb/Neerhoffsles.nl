fee518ad2bdb0218b62122995e35442587a53c3e
Oscar Rijpstra <00276969@rijnijssel.nl>
2018-04-22 19:09:21 +0200

    changed questionmarks

--- js/cur-sun.js
+++ js/cur-sun.js
@@ -7,3 +7,17 @@ window.addEventListener('scroll', () =>{
 function setSunPosition(){
     sun.style.transform = 'translateX(' + document.documentElement.scrollTop * 1.2 + '%)';
 }
+
+CurrentSun();
+  
+async function CurrentSunIntensity(){
+    const temp_span = document.getElementById('current-temp');
+    const res = await fetch('https://api.buienradar.nl/data/public/1.1/jsonfeed');
+    const json = await res.json();
+    const temp = json.buienradarnl.weergegevens.actueel_weer.weerstations.weerstation[1].temperatuurGC
+
+    temp_span.getElementsByTagName('span')[0].innerHTML = temp + ' C';
+}
+
+    
+
