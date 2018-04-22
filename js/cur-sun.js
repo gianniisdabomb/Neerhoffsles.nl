2c72b7d317137081b4f6ec63a2c736aef60d715d
Oscar Rijpstra <00276969@rijnijssel.nl>
2018-04-22 20:22:15 +0200

    added styling

--- js/cur-sun.js
+++ js/cur-sun.js
@@ -12,11 +12,13 @@ CurrentSun();
   
 async function CurrentSun(){
     const sunspan = document.getElementById('current-sun');
+    const curprice = document.getElementById('current-price')
     const res = await fetch('https://api.buienradar.nl/data/public/1.1/jsonfeed');
     const json = await res.json();
     const sun = json.buienradarnl.weergegevens.actueel_weer.weerstations.weerstation[1].zonintensiteitWM2
 
     sunspan.innerHTML = sun + ' Watt per M2';
+    curprice.innerHTML = '€ ' + Math.round(sun * 0.3) + ' per uur';
 }
 
     
